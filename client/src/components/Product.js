481babf55f3b95550878549859ff666a49adb824
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-02-18 21:31:12 +0200

    added

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// background: linear-gradient(180deg, #363941 0%, rgba(62, 66, 75, 0) 100%);
+// icons
+import { FiSearch } from 'react-icons/fi';
+
 const Product = ({ product }) => {
-  // console.log(product.attributes.price);
   return (
-    <div className='w-full h-[362px] bg-gradient-to-t from-[#1F2126] to-[#33363D] rounded-[8px] drop-shadow-2xl overflow-hidden mx-auto'>
-      {/* image */}
-      <div className='w-full h-[200px] flex items-center justify-center'>
-        <img
-          className='w-[160px] h-[160px]'
-          src={`http://localhost:1337${product.attributes.image.data.attributes.url}`}
-          alt=''
-        />
-      </div>
-      {/* text */}
-      <div className='px-6 pb-8 flex flex-col'>
-        {/* category title */}
-        <div className='text-sm text-accent capitalize mb-2'>
-          {product.attributes.categories.data[0].attributes.title}
+    <Link to={`/product/${product.id}`}>
+      <div className='w-full h-[362px] bg-gradient-to-t from-[#1F2126] to-[#33363D] rounded-[8px] drop-shadow-2xl overflow-hidden group'>
+        {/* image */}
+        <div className='w-full h-[200px] flex items-center justify-center relative'>
+          <img
+            className='w-[160px] h-[160px]'
+            src={`http://localhost:1337${product.attributes.image.data.attributes.url}`}
+            alt=''
+          />
         </div>
-        {/* title */}
-        <Link to={`/product/${product.id}`} className='mb-9'>
-          <div className='text-[15px]'>
+        {/* text */}
+        <div className='px-6 pb-8 flex flex-col'>
+          {/* category title */}
+          <div className='text-sm text-accent capitalize mb-2'>
+            {product.attributes.categories.data[0].attributes.title}
+          </div>
+          {/* title */}
+
+          <div className='text-[15px] mb-9'>
             {product.attributes.title.substring(0, 40)}...
           </div>
-        </Link>
-        {/* price */}
-        <div className='text-lg text-accent'>${product.attributes.price}</div>
+
+          {/* price */}
+          <div className='text-lg text-accent'>${product.attributes.price}</div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
 
 const Product = () => {
-  return <div>Product</div>;
+  const { id } = useParams();
+  const { data } = useFetch(`/products?populate=*&filters[id][$eq]=${id}`);
+  console.log(data);
+  return (
+    <div>
+      <div className='container mx-auto'>Product</div>
+    </div>
+  );
 };
 
 export default Product;
