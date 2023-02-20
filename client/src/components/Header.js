f8b06fdc18cc424c83b73b007afce5212c4911eb
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-02-20 21:24:46 +0200

    added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,42 @@
 import React from 'react';
+// images
+import Logo from '../img/logo.png';
+// icons
+import { FiSearch } from 'react-icons/fi';
+import { SlBag } from 'react-icons/sl';
+
+// link
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className='bg-primary mb-[30px]'>
-      <div className='container mx-auto'>header</div>
+    <header className='bg-primary mb-[30px] py-6'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row gap-4 lg:items-center justify-between'>
+          {/* logo */}
+          <Link to={'/'}>
+            <img src={Logo} alt='' />
+          </Link>
+          {/* search */}
+          <form className='relative w-full max-w-[734px]'>
+            <input
+              type='text'
+              placeholder='Search products...'
+              className='h-[50px] rounded-[8px] w-full outline-none px-6 text-primary'
+            />
+            <button className='btn btn-accent absolute top-0 right-0 rounded-tl-none rounded-bl-none'>
+              <FiSearch className='text-xl' />
+            </button>
+          </form>
+          {/* phone & cart */}
+          <div className='flex items-center gap-x-[30px]'>
+            <div>Need help? 1234 567 8910</div>
+            <div>
+              <SlBag className='text-2xl' />
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
