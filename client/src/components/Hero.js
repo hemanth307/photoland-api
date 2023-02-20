c68354f5f0c6e052a4504fc692f33fc8384a2192
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-02-20 17:18:44 +0200

    added

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -2,13 +2,16 @@ import React from 'react';
 // components
 import CategoryNav from './CategoryNav';
 import MainSlider from './MainSlider';
+// images
+import PromoImg1 from '../img/promo_img1.png';
+import PromoImg2 from '../img/promo_img2.png';
 
 const Hero = () => {
   return (
     <section>
       <div className='container mx-auto'>
         <div className='flex flex-col gap-y-[30px] xl:flex-row xl:gap-x-[30px]'>
-          {/* category sidebar */}
+          {/* sidebar */}
           <div>
             <CategoryNav />
           </div>
@@ -16,11 +19,39 @@ const Hero = () => {
           <div className='w-full max-w-[734px] mx-auto'>
             <MainSlider />
           </div>
-          <div className='flex flex-col gap-y-[30px] w-full max-w-sm mx-auto'>
+          <div className='flex flex-col gap-y-[30px] w-full max-w-sm mx-auto h-[500px]'>
             {/* promo 1 */}
-            <div className='grad flex-1 rounded-[8px]'>promo 1</div>
+            <div className='grad flex-1 h-[250px] rounded-[8px] overflow-hidden relative p-6'>
+              <div className='max-w-[144px] h-full flex flex-col justify-center'>
+                <div className='text-[20px] font-medium leading-tight mb-4'>
+                  SAVE 35% ALL DSLR CAMERAS
+                </div>
+                <a href='#' className='uppercase text-accent'>
+                  Shop now
+                </a>
+              </div>
+              <img
+                className='absolute z-20 -top-2 -right-4'
+                src={PromoImg1}
+                alt=''
+              />
+            </div>
             {/* promo 2 */}
-            <div className='grad flex-1 rounded-[8px]'>promo 2</div>
+            <div className='grad flex-1 h-[250px] rounded-[8px] overflow-hidden relative p-6'>
+              <div className='max-w-[144px] h-full flex flex-col justify-center'>
+                <div className='text-[20px] font-medium leading-tight mb-4'>
+                  SAVE 35% ALL DSLR CAMERAS
+                </div>
+                <a href='#' className='uppercase text-accent'>
+                  Shop now
+                </a>
+              </div>
+              <img
+                className='absolute z-20 top-4 -right-4'
+                src={PromoImg2}
+                alt=''
+              />
+            </div>
           </div>
         </div>
       </div>
--- client/src/components/MainSlider.js
+++ client/src/components/MainSlider.js
@@ -50,7 +50,7 @@ const MainSlider = () => {
       pagination={{
         clickable: true,
       }}
-      className='mainSlider h-full bg-primary xl:bg-mainSlider xl:bg-no-repeat max-w-sm lg:max-w-none rounded-[8px] overflow-hidden'
+      className='mainSlider h-full bg-primary xl:bg-mainSlider xl:bg-no-repeat max-w-sm lg:max-w-none rounded-[8px] overflow-hidden drop-shadow-2xl'
     >
       <>
         {sliderData.map((slide, index) => {
@@ -62,12 +62,14 @@ const MainSlider = () => {
                   <div className='uppercase mb-1 text-center lg:text-left'>
                     {slide.pretitle}
                   </div>
-                  <div className='text-3xl md:text-[46px] font-semibold uppercase leading-none text-center lg:text-left mb-24'>
+                  <div className='text-3xl md:text-[46px] font-semibold uppercase leading-none text-center lg:text-left mb-8 xl:mb-20'>
                     {slide.titlePart1} <br />
                     {slide.titlePart2} <br />
                     {slide.titlePart3}
                   </div>
-                  <button className='btn btn-accent'>Shop now</button>
+                  <button className='btn btn-accent mx-auto lg:mx-0'>
+                    Shop now
+                  </button>
                 </div>
                 {/* image */}
                 <div className='flex-1'>
