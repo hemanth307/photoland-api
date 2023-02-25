465d01395a0f80b080f433972dc23809d568726d
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-02-25 11:04:44 +0200

    added

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -6,27 +6,26 @@ import useFetch from '../hooks/useFetch';
 
 const Product = () => {
   const { id } = useParams();
-  const { data, isLoading } = useFetch(
-    `/products?populate=*&filters[id][$eq]=${id}`
-  );
-  console.log(data);
+  const { data } = useFetch(`/products?populate=*&filters[id][$eq]=${id}`);
 
   if (!data) {
     return <div>loading...</div>;
   }
 
   return (
-    <div>
+    <div className='mb-16 pt-44 lg:pt-[30px] xl:pt-0'>
       <div className='container mx-auto'>
-        <div className='flex gap-x-[30px] mb-16 pt-40 lg:pt-0'>
-          <div className='flex-1 grad p-8 rounded-[8px]'>
+        <div className='flex flex-col lg:flex-row gap-[30px]'>
+          <div className='flex-1 lg:max-w-[40%] grad rounded-lg flex justify-center items-center'>
             <img
               src={`http://localhost:1337${data[0].attributes.image.data.attributes.url}`}
               alt=''
+              className='w-full max-w-[80%]'
             />
           </div>
-          <div className='flex-1'>
-            <h2>{data[0].attributes.description}</h2>
+          <div className='flex-1 bg-primary p-12 xl:p-20 rounded-lg'>
+            <h2 className='h2 mb-4'>{data[0].attributes.title}</h2>
+            <p>{data[0].attributes.description}</p>
           </div>
         </div>
       </div>
